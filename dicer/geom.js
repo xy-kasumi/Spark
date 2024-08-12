e91027d3d09a4711662708e825391e8724e208ae
xyx <xanxys@gmail.com>
2024-08-12 14:55:06 +0900

    Refactor: separate pure processing module

---
+++ dicer/geom.js
@@ -0,0 +1,245 @@
+/**
+ * Pure geometry processing.
+ * Use of three.js in this module is limited to primitives (Vectors, Matrices).
+ */
+import { Vector2, Vector3 } from 'three';
+
+// voxel at (ix, iy, iz):
+// * occupies volume: [ofs + i * res, ofs + (i + 1) * res)
+// * has center: ofs + (i + 0.5) * res
+export class VoxelGrid {
+    constructor(ofs, res, numX, numY, numZ) {
+        this.ofs = ofs;
+        this.res = res;
+        this.numX = numX;
+        this.numY = numY;
+        this.numZ = numZ;
+        this.data = new Uint8Array(numX * numY * numZ);
+    }
+
+    clone() {
+        const vg = new VoxelGrid(this.ofs.clone(), this.res, this.numX, this.numY, this.numZ);
+        vg.data.set(this.data);
+        return vg;
+    }
+
+    sub(other) {
+        for (let i = 0; i < this.data.length; i++) {
+            this.data[i] = Math.max(0, this.data[i] - other.data[i]);
+        }
+        return this;
+    }
+
+    add(other) {
+        for (let i = 0; i < this.data.length; i++) {
+            this.data[i] = Math.min(255, this.data[i] + other.data[i]);
+        }
+        return this;
+    }
+
+    multiplyScalar(s) {
+        for (let i = 0; i < this.data.length; i++) {
+            this.data[i] = Math.round(this.data[i] * s);
+        }
+        return this;
+    }
+
+    set(ix, iy, iz, val) {
+        this.data[ix + iy * this.numX + iz * this.numX * this.numY] = val;
+    }
+
+    get(ix, iy, iz) {
+        return this.data[ix + iy * this.numX + iz * this.numX * this.numY];
+    }
+
+    count() {
+        let cnt = 0;
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i] !== 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    volume() {
+        return this.count() * this.res * this.res * this.res;
+    }
+}
+
+
+
+
+const isectLine = (p, q, z) => {
+    const d = q.z - p.z;
+    const t = (d === 0) ? 0.5 : (z - p.z) / d;
+    return p.clone().lerp(q, t);
+};
+
+const isectLine2 = (p, q, y) => {
+    const d = q.y - p.y;
+    const t = (d === 0) ? 0.5 : (y - p.y) / d;
+    return p.clone().lerp(q, t);
+};
+
+
+
+export const initVG = (surf, resMm) => {
+    const MARGIN_MM = 1;
+
+    // compute AABB
+    const aabbMin = new Vector3(surf[0], surf[1], surf[2]);
+    const aabbMax = new Vector3(surf[0], surf[1], surf[2]);
+    for (let i = 1; i < surf.length / 3; i++) {
+        const v = new Vector3(surf[3 * i + 0], surf[3 * i + 1], surf[3 * i + 2]);
+        aabbMin.min(v);
+        aabbMax.max(v);
+    }
+    console.log("AABB", aabbMin, aabbMax);
+
+    aabbMin.subScalar(MARGIN_MM);
+    aabbMax.addScalar(MARGIN_MM);
+    const numV = aabbMax.clone().sub(aabbMin).divideScalar(resMm).ceil();
+    console.log("VG size", numV);
+    return new VoxelGrid(aabbMin, resMm, numV.x, numV.y, numV.z);
+};
+
+export const diceSurf = (surf, vg) => {
+    console.log("dicing...");
+    for (let iz = 0; iz < vg.numZ; iz++) {
+        const sliceZ = vg.ofs.z + (iz + 0.5) * vg.res;
+        const cont = sliceSurfByPlane(surf, sliceZ);
+
+        for (let iy = 0; iy < vg.numY; iy++) {
+            const sliceY = vg.ofs.y + (iy + 0.5) * vg.res;
+            const bnds = sliceContourByLine(cont, sliceY);
+
+            let isOutside = true;
+            for (let ix = 0; ix < vg.numX; ix++) {
+                if (bnds.length === 0) {
+                    vg.set(ix, iy, iz, 0);
+                    continue;
+                }
+
+                const sliceX = vg.ofs.x + (ix + 0.5) * vg.res;
+
+                if (bnds[0] <= sliceX) {
+                    isOutside = !isOutside;
+                    bnds.shift();
+                }
+                vg.set(ix, iy, iz, isOutside ? 0 : 255);
+            }
+        }
+    }
+    console.log(`dicing done; volume: ${vg.volume()} mm^3 (${vg.count()} voxels)`);
+    return vg;
+};
+
+// contEdges: [x0, y0, x1, y1, ...]
+// returns: seg set [x0, x1, x2, ...]
+export const sliceContourByLine = (contEdges, sliceY) => {
+    const bnds = [];
+    const numEdges = contEdges.length / 4;
+    for (let i = 0; i < numEdges; i++) {
+        const p0 = new Vector2(contEdges[4 * i + 0], contEdges[4 * i + 1]);
+        const p1 = new Vector2(contEdges[4 * i + 2], contEdges[4 * i + 3]);
+
+        const s0 = Math.sign(p0.y - sliceY);
+        const s1 = Math.sign(p1.y - sliceY);
+
+        // early exit
+        if (s0 >= 0 && s1 >= 0) {
+            continue;
+        }
+        if (s0 < 0 && s1 < 0) {
+            continue;
+        }
+
+        const isect = isectLine2(p0, p1, sliceY);
+        bnds.push({ x: isect.x, isEnter: s0 >= 0 });
+    }
+    bnds.sort((a, b) => a.x - b.x);
+
+    const bndsClean = [];
+    let insideness = 0; // supports non-manifold, nested surfaces by allowing multiple enter.
+    bnds.forEach(b => {
+        if (b.isEnter) {
+            insideness++;
+            if (insideness === 1) {
+                bndsClean.push(b.x);
+            }
+        } else {
+            insideness--;
+            if (insideness === 0) {
+                bndsClean.push(b.x);
+            }
+            if (insideness < 0) {
+                console.error("Corrupt surface data (hole)");
+            }
+        }
+    });
+    if (insideness !== 0) {
+        console.error("Corrupt surface data (hole)");
+    }
+    if (bndsClean.length % 2 !== 0) {
+        bndsClean.pop();
+    }
+
+    return bndsClean;
+};
+
+
+// surfTris: [x0, y0, z0, x1, y1, z1, ...]
+// returns: contour edges
+export const sliceSurfByPlane = (surfTris, sliceZ) => {
+    const segs = [];
+
+    // tris are CCW.
+    const numTris = surfTris.length / 9;
+    for (let i = 0; i < numTris; i++) {
+        const p0 = new Vector3(surfTris[9 * i + 0], surfTris[9 * i + 1], surfTris[9 * i + 2]);
+        const p1 = new Vector3(surfTris[9 * i + 3], surfTris[9 * i + 4], surfTris[9 * i + 5]);
+        const p2 = new Vector3(surfTris[9 * i + 6], surfTris[9 * i + 7], surfTris[9 * i + 8]);
+
+        const s0 = Math.sign(p0.z - sliceZ);
+        const s1 = Math.sign(p1.z - sliceZ);
+        const s2 = Math.sign(p2.z - sliceZ);
+
+        // early exit
+        if (s0 >= 0 && s1 >= 0 && s2 >= 0) {
+            continue;
+        }
+        if (s0 < 0 && s1 < 0 && s2 < 0) {
+            continue;
+        }
+
+        // intersect 3 edges with
+        let up = null;
+        let down = null;
+        if (s0 < 0 && s1 >= 0) {
+            up = isectLine(p0, p1, sliceZ);
+        } else if (s0 >= 0 && s1 < 0) {
+            down = isectLine(p0, p1, sliceZ);
+        }
+
+        if (s1 < 0 && s2 >= 0) {
+            up = isectLine(p1, p2, sliceZ);
+        } else if (s1 >= 0 && s2 < 0) {
+            down = isectLine(p1, p2, sliceZ);
+        }
+
+        if (s2 < 0 && s0 >= 0) {
+            up = isectLine(p2, p0, sliceZ);
+        } else if (s2 >= 0 && s0 < 0) {
+            down = isectLine(p2, p0, sliceZ);
+        }
+
+        if (up === null || down === null) {
+            throw "Degenerate triangle";
+        }
+
+        segs.push(down.x, down.y, up.x, up.y); // down -> up is CCW contor in XY plane.
+    }
+
+    return segs;
+};
--- dicer/main.js
+++ dicer/main.js
@@ -3,74 +3,7 @@ import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
-// TODO: Refactor
-let objSurf = null;
-
-
-
-// voxel at (ix, iy, iz):
-// * occupies volume: [ofs + i * res, ofs + (i + 1) * res)
-// * has center: ofs + (i + 0.5) * res
-class VoxelGrid {
-    constructor(ofs, res, numX, numY, numZ) {
-        this.ofs = ofs;
-        this.res = res;
-        this.numX = numX;
-        this.numY = numY;
-        this.numZ = numZ;
-        this.data = new Uint8Array(numX * numY * numZ);
-    }
-
-    clone() {
-        const vg = new VoxelGrid(this.ofs.clone(), this.res, this.numX, this.numY, this.numZ);
-        vg.data.set(this.data);
-        return vg;
-    }
-
-    sub(other) {
-        for (let i = 0; i < this.data.length; i++) {
-            this.data[i] = Math.max(0, this.data[i] - other.data[i]);
-        }
-        return this;
-    }
-
-    add(other) {
-        for (let i = 0; i < this.data.length; i++) {
-            this.data[i] = Math.min(255, this.data[i] + other.data[i]);
-        }
-        return this;
-    }
-
-    multiplyScalar(s) {
-        for (let i = 0; i < this.data.length; i++) {
-            this.data[i] = Math.round(this.data[i] * s);
-        }
-        return this;
-    }
-
-    set(ix, iy, iz, val) {
-        this.data[ix + iy * this.numX + iz * this.numX * this.numY] = val;
-    }
-
-    get(ix, iy, iz) {
-        return this.data[ix + iy * this.numX + iz * this.numX * this.numY];
-    }
-
-    count() {
-        let cnt = 0;
-        for (let i = 0; i < this.data.length; i++) {
-            if (this.data[i] !== 0) {
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-
-    volume() {
-        return this.count() * this.res * this.res * this.res;
-    }
-}
+import { initVG, diceSurf, sliceSurfByPlane, sliceContourByLine } from './geom.js';
 
 
 const convGeomToSurf = (geom) => {
@@ -140,185 +73,13 @@ const diceLineAndVisualize = (surf, lineY, lineZ) => {
     view.visNonVg.push(visBnd);
 };
 
-const isectLine = (p, q, z) => {
-    const d = q.z - p.z;
-    const t = (d === 0) ? 0.5 : (z - p.z) / d;
-    return p.clone().lerp(q, t);
-};
-
-const isectLine2 = (p, q, y) => {
-    const d = q.y - p.y;
-    const t = (d === 0) ? 0.5 : (y - p.y) / d;
-    return p.clone().lerp(q, t);
-};
-
-
-const initVG = (surf, resMm) => {
-    const MARGIN_MM = 1;
-
-    // compute AABB
-    const aabbMin = new THREE.Vector3(surf[0], surf[1], surf[2]);
-    const aabbMax = new THREE.Vector3(surf[0], surf[1], surf[2]);
-    for (let i = 1; i < surf.length / 3; i++) {
-        const v = new THREE.Vector3(surf[3 * i + 0], surf[3 * i + 1], surf[3 * i + 2]);
-        aabbMin.min(v);
-        aabbMax.max(v);
-    }
-    console.log("AABB", aabbMin, aabbMax);
-
-    aabbMin.subScalar(MARGIN_MM);
-    aabbMax.addScalar(MARGIN_MM);
-    const numV = aabbMax.clone().sub(aabbMin).divideScalar(resMm).ceil();
-    console.log("VG size", numV);
-    return new VoxelGrid(aabbMin, resMm, numV.x, numV.y, numV.z);
-};
-
-const diceSurf = (surf, vg) => {
-    console.log("dicing...");
-    for (let iz = 0; iz < vg.numZ; iz++) {
-        const sliceZ = vg.ofs.z + (iz + 0.5) * vg.res;
-        const cont = sliceSurfByPlane(surf, sliceZ);
-
-        for (let iy = 0; iy < vg.numY; iy++) {
-            const sliceY = vg.ofs.y + (iy + 0.5) * vg.res;
-            const bnds = sliceContourByLine(cont, sliceY);
-
-            let isOutside = true;
-            for (let ix = 0; ix < vg.numX; ix++) {
-                if (bnds.length === 0) {
-                    vg.set(ix, iy, iz, 0);
-                    continue;
-                }
-
-                const sliceX = vg.ofs.x + (ix + 0.5) * vg.res;
-
-                if (bnds[0] <= sliceX) {
-                    isOutside = !isOutside;
-                    bnds.shift();
-                }
-                vg.set(ix, iy, iz, isOutside ? 0 : 255);
-            }
-        }
-    }
-    console.log(`dicing done; volume: ${vg.volume()} mm^3 (${vg.count()} voxels)`);
-    return vg;
-};
-
-// contEdges: [x0, y0, x1, y1, ...]
-// returns: seg set [x0, x1, x2, ...]
-const sliceContourByLine = (contEdges, sliceY) => {
-    const bnds = [];
-    const numEdges = contEdges.length / 4;
-    for (let i = 0; i < numEdges; i++) {
-        const p0 = new THREE.Vector2(contEdges[4 * i + 0], contEdges[4 * i + 1]);
-        const p1 = new THREE.Vector2(contEdges[4 * i + 2], contEdges[4 * i + 3]);
-
-        const s0 = Math.sign(p0.y - sliceY);
-        const s1 = Math.sign(p1.y - sliceY);
-
-        // early exit
-        if (s0 >= 0 && s1 >= 0) {
-            continue;
-        }
-        if (s0 < 0 && s1 < 0) {
-            continue;
-        }
-
-        const isect = isectLine2(p0, p1, sliceY);
-        bnds.push({ x: isect.x, isEnter: s0 >= 0 });
-    }
-    bnds.sort((a, b) => a.x - b.x);
-
-    const bndsClean = [];
-    let insideness = 0; // supports non-manifold, nested surfaces by allowing multiple enter.
-    bnds.forEach(b => {
-        if (b.isEnter) {
-            insideness++;
-            if (insideness === 1) {
-                bndsClean.push(b.x);
-            }
-        } else {
-            insideness--;
-            if (insideness === 0) {
-                bndsClean.push(b.x);
-            }
-            if (insideness < 0) {
-                console.error("Corrupt surface data (hole)");
-            }
-        }
-    });
-    if (insideness !== 0) {
-        console.error("Corrupt surface data (hole)");
-    }
-    if (bndsClean.length % 2 !== 0) {
-        bndsClean.pop();
-    }
-
-    return bndsClean;
-};
-
-
-// surfTris: [x0, y0, z0, x1, y1, z1, ...]
-// returns: contour edges
-const sliceSurfByPlane = (surfTris, sliceZ) => {
-    const segs = [];
-
-    // tris are CCW.
-    const numTris = surfTris.length / 9;
-    for (let i = 0; i < numTris; i++) {
-        const p0 = new THREE.Vector3(surfTris[9 * i + 0], surfTris[9 * i + 1], surfTris[9 * i + 2]);
-        const p1 = new THREE.Vector3(surfTris[9 * i + 3], surfTris[9 * i + 4], surfTris[9 * i + 5]);
-        const p2 = new THREE.Vector3(surfTris[9 * i + 6], surfTris[9 * i + 7], surfTris[9 * i + 8]);
-
-        const s0 = Math.sign(p0.z - sliceZ);
-        const s1 = Math.sign(p1.z - sliceZ);
-        const s2 = Math.sign(p2.z - sliceZ);
-
-        // early exit
-        if (s0 >= 0 && s1 >= 0 && s2 >= 0) {
-            continue;
-        }
-        if (s0 < 0 && s1 < 0 && s2 < 0) {
-            continue;
-        }
-
-        // intersect 3 edges with
-        let up = null;
-        let down = null;
-        if (s0 < 0 && s1 >= 0) {
-            up = isectLine(p0, p1, sliceZ);
-        } else if (s0 >= 0 && s1 < 0) {
-            down = isectLine(p0, p1, sliceZ);
-        }
-
-        if (s1 < 0 && s2 >= 0) {
-            up = isectLine(p1, p2, sliceZ);
-        } else if (s1 >= 0 && s2 < 0) {
-            down = isectLine(p1, p2, sliceZ);
-        }
-
-        if (s2 < 0 && s0 >= 0) {
-            up = isectLine(p2, p0, sliceZ);
-        } else if (s2 >= 0 && s0 < 0) {
-            down = isectLine(p2, p0, sliceZ);
-        }
-
-        if (up === null || down === null) {
-            throw "Degenerate triangle";
-        }
-
-        segs.push(down.x, down.y, up.x, up.y); // down -> up is CCW contor in XY plane.
-    }
-
-    return segs;
-};
 
 const loadStl = (fname) => {
     const loader = new STLLoader();
     loader.load(
         `models/${fname}.stl`,
-        function (geometry) {
-            objSurf = convGeomToSurf(geometry);
+        (geometry) => {
+            dicer.objSurf = convGeomToSurf(geometry);
 
             const material = new THREE.MeshPhysicalMaterial({
                 color: 0xb2ffc8,
@@ -344,6 +105,18 @@ const loadStl = (fname) => {
             console.log(error);
         }
     );
+};
+
+const generateBlankGeom = () => {
+    const blankRadius = 25;
+    const blankHeight = 50;
+    const geom = new THREE.CylinderGeometry(blankRadius, blankRadius, blankHeight, 64, 1);
+    const transf = new THREE.Matrix4().compose(
+        new THREE.Vector3(0, 0, blankHeight / 2),
+        new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI / 2),
+        new THREE.Vector3(1, 1, 1));
+    geom.applyMatrix4(transf);
+    return geom;
 };
 
 
@@ -414,18 +187,6 @@ const createVgVis = (vg) => {
     return mesh;
 };
 
-const generateBlankGeom = () => {
-    const blankRadius = 25;
-    const blankHeight = 50;
-    const geom = new THREE.CylinderGeometry(blankRadius, blankRadius, blankHeight, 64, 1);
-    const transf = new THREE.Matrix4().compose(
-        new THREE.Vector3(0, 0, blankHeight / 2),
-        new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI / 2),
-        new THREE.Vector3(1, 1, 1));
-    geom.applyMatrix4(transf);
-    return geom;
-};
-
 const generateBlank = () => {
     const blank = new THREE.Mesh(
         generateBlankGeom(),
@@ -453,6 +214,10 @@ const generateTool = () => {
 
     return needle;
 };
+
+
+////////////////////////////////////////////////////////////////////////////////
+// 3D view
 
 class View3D {
     constructor() {
@@ -546,6 +311,34 @@ class View3D {
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// GUI
+
+const Model = {
+    HELICAL_GEAR: "helical_gear",
+    DICE_TOWER: "dice_tower",
+};
+
+const dicer = {
+    model: Model.HELICAL_GEAR,
+    resMm: 0.5,
+    lineZ: 1,
+    lineY: 0,
+    showVoxels: false,
+    objSurf: null,
+    dice: () => {
+        const blankSurf = convGeomToSurf(generateBlankGeom());
+        diceAndVisualize(blankSurf, dicer.objSurf, dicer.resMm);
+    },
+    diceLine: () => {
+        const sf = convGeomToSurf(generateBlankGeom());
+        diceLineAndVisualize(dicer.objSurf, dicer.lineY, dicer.lineZ);
+    },
+    toolX: 0,
+    toolY: 0,
+    toolZ: 0,
+};
+
 function initGui(view) {
     const gui = new GUI();
     gui.add(dicer, 'model', Model).onChange((model) => {
@@ -576,34 +369,11 @@ function initGui(view) {
     gui.add(dicer, "toolX", -50, 50).step(0.1).onChange(v => view.tool.position.x = v);
     gui.add(dicer, "toolY", -50, 50).step(0.1).onChange(v => view.tool.position.y = v);
     gui.add(dicer, "toolZ", 0, 100).step(0.1).onChange(v => view.tool.position.z = v);
+
+    loadStl(dicer.model);
 }
 
-
-const Model = {
-    HELICAL_GEAR: "helical_gear",
-    DICE_TOWER: "dice_tower",
-};
-
-const dicer = {
-    model: Model.HELICAL_GEAR,
-    resMm: 0.5,
-    lineZ: 1,
-    lineY: 0,
-    showVoxels: false,
-    dice: () => {
-        const blankSurf = convGeomToSurf(generateBlankGeom());
-        diceAndVisualize(blankSurf, objSurf, dicer.resMm);
-    },
-    diceLine: () => {
-        const sf = convGeomToSurf(generateBlankGeom());
-        diceLineAndVisualize(objSurf, dicer.lineY, dicer.lineZ);
-    },
-    toolX: 0,
-    toolY: 0,
-    toolZ: 0,
-};
-
-
-loadStl(dicer.model);
+////////////////////////////////////////////////////////////////////////////////
+// entry point
 const view = new View3D();
 initGui(view);
