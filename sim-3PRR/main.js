7f5809573bfc09731433c20b361f0c0e52d6cdc5
xyx <xanxys@gmail.com>
2024-09-18 22:39:43 +0900

    State space scan

--- sim-3PRR/main.js
+++ sim-3PRR/main.js
@@ -1,7 +1,9 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.168.0/+esm";
 
 const canvas = document.getElementById('mechanismCanvas');
-const ctx = canvas.getContext('2d');
+const canvasIn = document.getElementById('inputSpaceCanvas');
+const canvasOut = document.getElementById('outputSpaceCanvas');
+
 const sliders = [
     document.getElementById('slider1'),
     document.getElementById('slider2'),
@@ -11,18 +13,35 @@ const sliders = [
 const linkLength = 50;
 const effectorLength = 25;
 
+let eLPrev = null;
+let eRPrev = null;
+const samples = [];
+
 function computePositions(inputs) {
     const p1 = new THREE.Vector2(inputs[0], 0);
     const p2 = new THREE.Vector2(inputs[1], 0);
     const p3 = new THREE.Vector2(inputs[2], 0);
-    const eL = solveTriangle(p1, linkLength, p2, linkLength, new THREE.Vector2(50, 50));
+    const eL = solveTriangle(p1, linkLength, p2, linkLength, eLPrev || new THREE.Vector2(50, 50));
+    eLPrev = eL;
     if (eL === null) {
         return {links: []};
     }
-    const eR = solveTriangle(eL, effectorLength, p3, linkLength, new THREE.Vector2(50, 50));
+    const eR = solveTriangle(eL, effectorLength, p3, linkLength, eRPrev || new THREE.Vector2(50, 50));
+    eRPrev = eR;
     if (eR === null) {
         return {links: []};
     }
+
+    const delta = eR.clone().sub(eL).normalize();
+
+    samples.push({
+        i0: inputs[0],
+        i1: inputs[1],
+        i2: inputs[2],
+        x: eL.x,
+        y: eL.y,
+        t: Math.atan2(delta.y, delta.x) * 180 / Math.PI,
+    });
 
     return {
         links: [
@@ -57,6 +76,7 @@ function solveTriangle(p, lenP, q, lenQ, near) {
 }
 
 function render(positions) {
+    const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     try {
@@ -88,11 +108,72 @@ function render(positions) {
     }
 }
 
+
+function renderOutput(samples) {
+    const ctx = canvasOut.getContext('2d');
+    ctx.clearRect(0, 0, canvasOut.width, canvasOut.height);
+
+    ctx.fillStyle = 'black';
+    console.log(samples);
+    samples.forEach(sample => {
+        
+        ctx.beginPath();
+        ctx.arc(sample.y + 100, sample.t / 180 * 100 + 100, 1, 0, 2 * Math.PI);
+        ctx.fill();
+    });
+}
+
+
+function renderInput(samples) {
+    const ctx = canvasIn.getContext('2d');
+    ctx.clearRect(0, 0, canvasIn.width, canvasIn.height);
+
+    ctx.fillStyle = 'black';
+    console.log(samples);
+    samples.forEach(sample => {
+        
+        ctx.beginPath();
+        ctx.arc(sample.i1 + 100, sample.i2 + 100, 1, 0, 2 * Math.PI);
+        ctx.fill();
+    });
+}
+
 function update() {
     const inputs = sliders.map(slider => parseFloat(slider.value));
     const positions = computePositions(inputs);
     render(positions);
+    renderInput(samples);
+    renderOutput(samples);
 }
 
 sliders.forEach(slider => slider.addEventListener('input', update));
 update(); // Initial render
+
+document.getElementById("resetButton").addEventListener("click", () => {
+    samples.clear();
+});
+
+const playN = 30;
+let playIx0 = 0;
+let playIx1 = 0;
+
+function execStep() {
+    sliders[1].value = 11 + 90 * (playIx0 / playN);
+    sliders[2].value = 11 + 90 * (playIx1 / playN);
+    update();
+
+    playIx0++;
+    if (playIx0 >= playN) {
+        playIx0 = 0;
+        playIx1++;
+        if (playIx1 >= playN) {
+            playIx1 = 0;
+            return;
+        }
+    }
+    setTimeout(execStep, 5);
+}
+
+document.getElementById("playButton").addEventListener("click", () => {
+    execStep();
+});
